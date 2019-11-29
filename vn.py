35b5e8c79ca2f9adfad66aa951d0d3c9c94da6e9
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2019-11-29 03:41:54 +0100

    Wrote gvns, but it doesn't work. It works if I tweak it so it uses .next instead of .random, so I think the error is in .random

---
+++ vn.py
@@ -0,0 +1,37 @@
+from search import local_search, best_improvement
+import copy
+from neighborhood import n_step
+import logging
+
+def vnd(solution, neighborhood_factory):
+    print('Starting VND...')
+    l = 0
+    best = solution
+    while l < 4:
+        neighborhood_factory.set_index(l)
+        new = local_search(best, best_improvement, neighborhood_factory, 1)
+        if new < best:
+            best = new
+            print('New best solution found: {}'.format(best.obj))
+            l = 0
+        else:
+            l += 1
+    return best
+
+
+def gvns(init_sol,neighborhood_fac, vnd_neighborhood_fac, k_max, iter):
+    best_sol = init_sol
+    i = 0
+    while i < iter:
+        k = 1
+        while k < k_max:
+            logging.debug("bla " + str(k))
+            random_sol = n_step(best_sol, neighborhood_fac, k)
+            vnd_sol = vnd(random_sol, vnd_neighborhood_fac)
+            if vnd_sol < best_sol:
+                best_sol = vnd_sol
+                k = 1
+            else:
+                k = k+1
+        i += 1
+    return best_sol
